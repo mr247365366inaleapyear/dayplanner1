9ae018540a3446cb4cf8d57d8f60dc95310ba091
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-01-04 08:22:19 -0500

    added script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+button9.on('click', function() {
+    event.preventDefault();
+    if (box9.val() !== '') {
+        localStorage.setItem('Entry9', box9.val());
+    }
+});
+button10.on('click', function() {
+    event.preventDefault();
+    if (box10.val() !== '') {
+        localStorage.setItem('Entry10', box10.val());
+    }
+});
+button11.on('click', function() {
+    event.preventDefault();
+    if (box11.val() !== '') {
+        localStorage.setItem('Entry11', box11.val());
+    }
+});
+button12.on('click', function() {
+    event.preventDefault();
+    if (box12.val() !== '') {
+        localStorage.setItem('Entry12', box12.val());
+    }
+});
+button1.on('click', function() {
+    event.preventDefault();
+    if (box1.val() !== '') {
+        localStorage.setItem('Entry1', box1.val());
+    }
+});
+button2.on('click', function() {
+    event.preventDefault();
+    if (box2.val() !== '') {
+        localStorage.setItem('Entry2', box2.val());
+    }
+});
+button3.on('click', function() {
+    event.preventDefault();
+    if (box3.val() !== '') {
+        localStorage.setItem('Entry3', box3.val());
+    }
+});
+button4.on('click', function() {
+    event.preventDefault();
+    if (box4.val() !== '') {
+        localStorage.setItem('Entry4', box4.val());
+    }
+});
+button5.on('click', function() {
+    event.preventDefault();
+    if (box5.val() !== '') {
+        localStorage.setItem('Entry5', box5.val());
+    }
+});
